2607e101555bea25abba30d7daab098a5587281a
Jérôme BATAILLE <mail@jeromebataille.fr>
2010-05-19 05:11:21 +0200

    Page d'admin des traductions 	new file:   app/controllers/admin_translations_controller.rb 	modified:   app/views/admin_menu/index.html.erb 	new file:   app/views/admin_translations/index.html.erb 	modified:   config/routes.rb

---
+++ app/controllers/admin_translations_controller.rb
@@ -0,0 +1,8 @@
+class AdminTranslationsController < ApplicationController
+	def show
+		respond_to do |format|
+			format.html # show.html.erb
+		end
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :commands
 
   map.admin '/admin/inflexions', :controller => 'admin_inflexions'
+  map.admin '/admin/translations', :controller => 'admin_translations'
   map.admin '/admin/dev_infos', :controller => 'admin_dev_infos'
+
   map.admin '/admin/routes_infos', :controller => 'admin_routes_infos'
   map.admin '/admin/commands', :controller => 'commands'
+
+  map.admin '/admin/send_file', :controller => 'admin_send_file'
 end
