3d752104025ba9cbc85ed9ffc8afb3e45305123a
Jérôme BATAILLE <mail@jeromebataille.fr>
2010-09-08 14:59:56 +0200

    Ajout Infos Url 	modified:   README 	new file:   app/controllers/admin_url_infos_controller.rb 	new file:   app/views/admin_url_infos/index.html.erb 	modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ ActionController::Routing::Routes.draw do |map|
 	#Le menu
 	map.admin '/admin_menu', :controller => 'admin_menu'
 
+	#Les outils
 	map.admin '/admin/inflexions', :controller => 'admin_inflexions'
 	map.admin '/admin/translations', :controller => 'admin_translations'
 	map.admin '/admin/dev_infos', :controller => 'admin_dev_infos'
+	map.admin '/admin/url_infos', :controller => 'admin_url_infos'
 
 	map.admin '/admin/routes_infos', :controller => 'admin_routes_infos'
 	map.admin '/admin/commands', :controller => 'commands'
