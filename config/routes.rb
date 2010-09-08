32ec77efc16a62cef994cf97bd093b3055dc37f6
Jérôme BATAILLE <mail@jeromebataille.fr>
2010-09-08 14:42:09 +0200

    ajout ressource menu admin_menu 	modified:   config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :commands
+	map.resources :commands
 
-  map.admin '/admin/inflexions', :controller => 'admin_inflexions'
-  map.admin '/admin/translations', :controller => 'admin_translations'
-  map.admin '/admin/dev_infos', :controller => 'admin_dev_infos'
+	#Le menu
+	map.admin '/admin_menu', :controller => 'admin_menu'
 
-  map.admin '/admin/routes_infos', :controller => 'admin_routes_infos'
-  map.admin '/admin/commands', :controller => 'commands'
+	map.admin '/admin/inflexions', :controller => 'admin_inflexions'
+	map.admin '/admin/translations', :controller => 'admin_translations'
+	map.admin '/admin/dev_infos', :controller => 'admin_dev_infos'
 
-  map.admin '/admin/send_file', :controller => 'admin_send_file'
+	map.admin '/admin/routes_infos', :controller => 'admin_routes_infos'
+	map.admin '/admin/commands', :controller => 'commands'
+
+	map.admin '/admin/send_file', :controller => 'admin_send_file'
 end
