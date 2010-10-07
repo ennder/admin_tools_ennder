719e666e6c207eb54f36999df99f6f08580e5f05
Ennder <mel@ennder.fr>
2010-10-07 14:48:28 +0200

    ajour de la version de rails dans le menu déport code dans le controlleur 	modified:   app/controllers/admin_translations_controller.rb 	modified:   app/views/admin_menu/index.html.erb 	modified:   app/views/admin_translations/index.html.erb

--- app/controllers/admin_translations_controller.rb
+++ app/controllers/admin_translations_controller.rb
@@ -1,5 +1,7 @@
 class AdminTranslationsController < ApplicationController
 	def show
+		@traductions_all = I18n.backend.send(:translations)
+
 		respond_to do |format|
 			format.html # show.html.erb
 		end
