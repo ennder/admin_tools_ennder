2de7bfa0a8fce25e2fcf3eb47af831a89401596c
Ennder <mel@ennder.fr>
2010-10-07 13:32:39 +0200

    Ajout de tests : sur le paramètre et sur l'existence de la classe 	modified:   app/controllers/admin_dev_infos_controller.rb

--- app/controllers/admin_dev_infos_controller.rb
+++ app/controllers/admin_dev_infos_controller.rb
@@ -1,8 +1,21 @@
 class AdminDevInfosController < ApplicationController
 	def show
-		if params[:nom_objet] != ''
-			@l_objet = Object::const_get(params[:nom_objet]).new()
+		if params[:nom_objet]?blank?
+			flash[:notice] = 'Il faut spécifier un nom de Classe'
+			redirect_to( :action => :index )
+
+			return
 		end
+
+		_La_classe = Object::const_get(params[:nom_objet])
+		if _La_classe.nil?
+			flash[:notice] = "La Classe #{params[:nom_objet]} n'existe pas"
+			redirect_to( :action => :index )
+
+			return
+		end
+
+		@l_objet = _La_classe.new()
 
 		respond_to do |format|
 			format.html # show.html.erb
