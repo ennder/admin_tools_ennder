31d39d80d327174de4a7e24112629b76b26e2b78
Ennder <mel@ennder.fr>
2010-10-07 14:57:10 +0200

    trace nb traductions 	modified:   app/controllers/admin_translations_controller.rb

--- app/controllers/admin_translations_controller.rb
+++ app/controllers/admin_translations_controller.rb
@@ -1,6 +1,7 @@
 class AdminTranslationsController < ApplicationController
 	def index
 		@traductions_all = I18n.backend.send(:translations)
+		logger.debug "[#{@traductions_all.size}] traduction(s)"
 
 		respond_to do |format|
 			format.html # index.html.erb
