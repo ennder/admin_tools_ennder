d5d664eb91e830a43266df2452e5d07cf496a3c7
Ennder <mel@ennder.fr>
2011-01-03 00:00:20 +0100

    Chargées dynamiquement 	deleted:    config/routes.rb 	new file:   config/routes_custom.rb

---
+++ config/routes_custom.rb
@@ -0,0 +1,17 @@
+ActionController::Routing::Routes.draw do |map|
+	map.resources :commands
+
+	#Le menu
+	map.admin '/admin_menu', :controller => 'admin_menu'
+
+	#Les outils
+	map.admin '/admin/inflexions',		:controller => 'admin_inflexions'
+	map.admin '/admin/translations',	:controller => 'admin_translations'
+	map.admin '/admin/routes',			:controller => 'admin_routes'
+
+	map.admin '/admin/dev_infos',		:controller => 'admin_dev_infos'
+	map.admin '/admin/url_infos',		:controller => 'admin_url_infos'
+
+	map.admin '/admin/commands',		:controller => 'commands'
+	map.admin '/admin/send_file',		:controller => 'admin_send_file'
+end
