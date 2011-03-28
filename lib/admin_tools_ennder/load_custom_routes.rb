e6beb7fb0528d31687d7876cf4ac213680c8d727
Ennder <mel@ennder.fr>
2011-03-28 04:15:37 +0200

    test version rails < 3 	modified:   lib/admin_tools_ennder/load_custom_routes.rb

--- lib/admin_tools_ennder/load_custom_routes.rb
+++ lib/admin_tools_ennder/load_custom_routes.rb
@@ -1,1 +1,3 @@
-ActionController::Routing::Routes.add_configuration_file( File.join(::File.dirname(__FILE__), '..', 'config', 'routes_custom.rb') ) 
+if Rails::VERSION::MAJOR < 3
+	ActionController::Routing::Routes.add_configuration_file( File.join(::File.dirname(__FILE__), '..', '..', 'config', 'routes.rb') )
+end
