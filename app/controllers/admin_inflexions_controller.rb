e0b26d301c625550271a9ffcf23419054faf6921
Ennder <mel@ennder.fr>
2011-03-28 04:14:39 +0200

    calcul @inflections_plur @inflections_sing 	modified:   app/controllers/admin_inflexions_controller.rb

--- app/controllers/admin_inflexions_controller.rb
+++ app/controllers/admin_inflexions_controller.rb
@@ -1,2 +1,6 @@
 class AdminInflexionsController < ApplicationController
+	def index
+		@inflections_plur = ActiveSupport::Inflector.inflections.plurals
+		@inflections_sing = ActiveSupport::Inflector.inflections.singulars
+	end
 end
