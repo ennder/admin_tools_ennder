7e4a04a042a230a63729059abf930b3f714e2d64
Ennder <mel@ennder.fr>
2011-03-28 04:13:11 +0200

    accents => tag utf-8 vérification retour 	I18n.backend.send(:translations) 	modified:   app/controllers/admin_translations_controller.rb

--- app/controllers/admin_translations_controller.rb
+++ app/controllers/admin_translations_controller.rb
@@ -1,7 +1,13 @@
+# -*- coding: utf-8 -*-
 class AdminTranslationsController < ApplicationController
 	def index
-		#TODO correction : I18n.backend.send(:translations) renvoie une liste vide
-		@traductions_all = I18n.backend.send(:translations)
+		#TODO Ennder correction : Rails 3 ? I18n.backend.send(:translations) renvoie une liste vide
+		begin
+			@traductions_all = I18n.backend.send(:translations)
+		rescue _exception
+			@traductions_all = []
+			logger.error "échec I18n.backend.send(:translations) [#{_exception.message}]"
+		end
 		logger.debug "[#{@traductions_all.size}] traduction(s)"
 
 		respond_to do |format|
