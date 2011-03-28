34968cb0f5c0ab77c4f42f05bcee31ff0c4d368f
Ennder <mel@ennder.fr>
2011-03-28 04:05:10 +0200

    coding utf8 (accents) . à la fin des messages de flash 	modified:   app/controllers/admin_dev_infos_controller.rb

--- app/controllers/admin_dev_infos_controller.rb
+++ app/controllers/admin_dev_infos_controller.rb
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
 class AdminDevInfosController < ApplicationController
 	def show
 		if params[:nom_objet].blank?
-			flash[:notice] = 'Il faut spécifier un nom de Classe'
+			flash[:notice] = 'Il faut spécifier un nom de Classe.'
 			redirect_to( :action => :index )
 
 			return
@@ -15,7 +16,7 @@ class AdminDevInfosController < ApplicationController
 		end
 
 		if ! _defini
-			flash[:notice] = "La Classe #{params[:nom_objet]} n'existe pas"
+			flash[:notice] = "La Classe #{params[:nom_objet]} n'existe pas."
 			redirect_to( :action => :index )
 
 			return
@@ -24,7 +25,7 @@ class AdminDevInfosController < ApplicationController
 		_La_classe = Object::const_get(params[:nom_objet])
 
 		if ! _La_classe.respond_to?(:new)
-			flash[:notice] = "La Classe #{_La_classe} ne fournit pas la méthode new()"
+			flash[:notice] = "La Classe #{_La_classe} ne fournit pas la méthode new()."
 			redirect_to( :action => :index )
 
 			return
