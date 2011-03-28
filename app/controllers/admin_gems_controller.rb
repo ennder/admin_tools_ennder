b6b9caa5132142c56e57d410d5905d31e4e24dad
Ennder <mel@ennder.fr>
2011-03-28 23:47:34 +0200

    Doit marcher avec Rails3 	modified:   app/controllers/admin_gems_controller.rb

--- app/controllers/admin_gems_controller.rb
+++ app/controllers/admin_gems_controller.rb
@@ -1,2 +1,15 @@
+if Rails::VERSION::MAJOR >= 3
+	require 'bundler'
+end
+
+#mygems = Bundler.load.specs.map { |spec| spec.name }
+
 class AdminGemsController < ApplicationController
+	def index
+		if Rails::VERSION::MAJOR < 3
+			@application_gems = Rails.configuration.gems
+		else
+			@application_gems = Bundler.load.specs
+		end
+	end
 end
