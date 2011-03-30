548b59be616658575fdd93b365a046fedab8f039
Ennder <mel@ennder.fr>
2011-03-30 21:54:00 +0200

    renamed:    app/metal/processus_liste.rb -> app/metal/metal_processus_liste.rb

--- app/metal/metal_processus_liste.rb
+++ app/metal/metal_processus_liste.rb
@@ -2,9 +2,9 @@
 # Allow the metal piece to run in isolation
 require(File.dirname(__FILE__) + "/../../config/environment") unless defined?(Rails)
 
-class ProcessusListe
+class MetalProcessusListe
   def self.call(env)
-    if env["PATH_INFO"] =~ /^\/admin\/processus_liste/
+    if env["PATH_INFO"] =~ /^\/admin\/metal_processus_liste/
       [200, {"Content-Type" => "text/html"}, [Time.new.strftime("%Y-%m-%d_%H:%M:%S") + '<br/>' + `ps -edfH`]]
     else
       [404, {"Content-Type" => "text/html"}, ["Pas Trouvé"]]
