dbd967444a99010982cec3f03e5bb363022f7889
Ennder <mel@ennder.fr>
2011-03-30 21:54:56 +0200

    l'engine marche avec Rails 2 aussi ! charge les routes dans config/ masi bas besoin de la classe engine !!! 	modified:   lib/admin_tools_ennder/load_custom_routes.rb

--- lib/admin_tools_ennder/load_custom_routes.rb
+++ lib/admin_tools_ennder/load_custom_routes.rb
@@ -1,3 +1,3 @@
-if Rails::VERSION::MAJOR < 3
-	ActionController::Routing::Routes.add_configuration_file( File.join(::File.dirname(__FILE__), '..', '..', 'config', 'routes.rb') )
-end
+#if Rails::VERSION::MAJOR < 3
+#	ActionController::Routing::Routes.add_configuration_file( File.join(::File.dirname(__FILE__), '..', '..', 'config', 'routes.rb') )
+#end
