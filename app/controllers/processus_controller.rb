cf6ecd2e2ab72323f94a503dac2f43f2aa641c38
Ennder <mel@ennder.fr>
2011-03-30 21:58:51 +0200

    action index_refreshed => index 	modified:   app/controllers/processus_controller.rb

--- app/controllers/processus_controller.rb
+++ app/controllers/processus_controller.rb
@@ -1,8 +1,8 @@
 class ProcessusController < ApplicationController
   def index
-    render :text => Time.new.strftime("%Y-%m-%d_%H:%M:%S") + '<br/>' + `ps -edfH`
   end
 
-  def index_refreshed
+  def index_static
+    render :text => Time.new.strftime("%Y-%m-%d_%H:%M:%S") + '<br/>' + `ps -edfH`
   end
 end
