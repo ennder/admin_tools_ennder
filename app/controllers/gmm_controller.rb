ffcfdc48ab3e10d288f8f2a162dc1945463e8619
Ennder <mel@ennder.fr>
2014-02-23 00:27:23 +0100

    load all application models 	modifié :   app/controllers/gmm_controller.rb

--- app/controllers/gmm_controller.rb
+++ app/controllers/gmm_controller.rb
@@ -1,4 +1,13 @@
 class GmmController < ApplicationController
 	def index
+		# Load any model classes
+		Dir[Rails.root.to_s + '/app/models/**/*.rb'].each do |file|
+			begin
+#				puts "loading #{file}"
+				require file
+			rescue
+				Rails.logger.error "GTM: Error loading #{file}"
+			end
+		end
 	end
 end
