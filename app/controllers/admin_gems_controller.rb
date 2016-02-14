a39268fdf5af32cd25fc7059524c53c084609e07
Ennder <mel@ennder.fr>
2016-02-14 16:12:11 +0100

    tri liste des gems modifié :         app/controllers/admin_gems_controller.rb modifié :         app/views/admin_gems/_gem_infos.html.erb

--- app/controllers/admin_gems_controller.rb
+++ app/controllers/admin_gems_controller.rb
@@ -2,14 +2,23 @@ if Rails::VERSION::MAJOR >= 3
 	require 'bundler'
 end
 
-#mygems = Bundler.load.specs.map { |spec| spec.name }
-
 class AdminGemsController < ApplicationController
 	def index
+		#mygems = Bundler.load.specs.map { |spec| spec.name }
+
 		if Rails::VERSION::MAJOR < 3
-			@application_gems = Rails.configuration.gems
+			application_gems = Rails.configuration.gems
 		else
-			@application_gems = Bundler.load.specs
+			application_gems = Bundler.load.specs
 		end
+
+		@application_gems = application_gems.collect{|g|
+			[
+				g.name,
+				( (Rails::VERSION::MAJOR < 3) ? g.requirement : g.version.to_s ) || '?'
+			]
+		}.sort!{|a, b|
+			a[0] <=> b[0]
+		}
 	end
 end
