1e236602ba6c42459149a1878fcb8d2a360dfb7e
Ennder <mel@ennder.fr>
2016-02-14 15:02:49 +0100

    correction modifié :         app/helpers/application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,10 @@
 module ApplicationHelper
 	if Rails::VERSION::MAJOR >= 3
 		def periodically_call_remote(options = {})
-			frequency = options[:frequency] || 10 # every ten seconds by
-			default
+			frequency = options[:frequency] || 10 # every ten seconds by default
+
 			code = "new PeriodicalExecuter(function() {#{remote_function(options)}}, #{frequency})"
+
 			javascript_tag(code)
 		end
 	end
