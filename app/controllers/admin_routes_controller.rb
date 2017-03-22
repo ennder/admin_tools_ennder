cff16d6bf0e8502b4e32c157fb1ff0053da3c5cb
Ennder <mel@ennder.fr>
2017-03-22 15:21:52 +0100

    Compatibilé avec Rails 5
    modifié :         app/controllers/admin_routes_controller.rb
    modifié :         app/views/admin_routes/index.html.erb

--- app/controllers/admin_routes_controller.rb
+++ app/controllers/admin_routes_controller.rb
@@ -1,2 +1,21 @@
 class AdminRoutesController < ApplicationController
+	def index
+		if Rails::VERSION::MAJOR >= 3
+			all_routes = Rails.application.routes
+		else
+			all_routes = ActionController::Routing::Routes
+		end
+
+		if all_routes.named_routes.respond_to?(:routes)
+			@routes_hash = all_routes.routes
+			@named_routes_hash = all_routes.named_routes.routes
+		else
+			@routes_hash = all_routes.routes.to_a
+			@named_routes_hash = all_routes.named_routes.to_a
+		end
+
+		respond_to do |format|
+			format.html # index.html.erb
+		end
+	end
 end
